e458459e6ee873a140ee926b5ca0e8d004a2b67a
EpashAI <104898898+TheBochok@users.noreply.github.com>
2025-03-16 13:37:42 +0200

    Add another incorrect usage example

--- usage.py
+++ usage.py
@@ -2,4 +2,7 @@ import requests
 
 base_url = "http://localhost"
 
-meme_image_url = requests.post(f"{base_url}/generate-meme", data={})
+meme_image_url = requests.post(f"{base_url}/generate-meme", data={}).json()
+meme_image_url = requests.post(
+    f"{base_url}/generate-meme", data={"meme_top": "typo in this param"}
+).json()
